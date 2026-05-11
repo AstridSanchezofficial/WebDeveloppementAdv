4e29a7c55366a913ffddb9b7f7afd25f75175cc1
astrid.Sanchez <omid.panahi@vaniercollege.qc.ca>
2026-05-11 17:53:08 -0400

    adding instance methods borrow(), return_boook() and display_info()

--- lab2/library-item-tracker/book.py
+++ lab2/library-item-tracker/book.py
@@ -4,4 +4,11 @@ class Book:
         self.author=author
         self.available=available
 
-        
+    def borrow(self):
+        self.available=False
+
+    def return_book(self):
+        self.available=True
+    
+    def display_info(self):
+        print(f"Book's name:{self.title}, author:{self.author}, availability:{self.available}")
