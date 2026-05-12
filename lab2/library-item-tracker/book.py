bb023221cc8bfd93c11d823d4fd156cc2c63be0c
astrid.Sanchez <omid.panahi@vaniercollege.qc.ca>
2026-05-11 21:32:51 -0400

    Adding classmethod and static method

--- lab2/library-item-tracker/book.py
+++ lab2/library-item-tracker/book.py
@@ -1,7 +1,19 @@
 class Book:
+    library_name="Central Library"
     def __init__(self,title,author,available):
         self.title=title
         self.author=author
         self.available=available
 
+    @classmethod
+    def change_library_name(cls,new_library):
+       cls.library_name = new_library        
+        
+    @staticmethod
+
+    def is_valid_title(title):
+        if title =="":
+             print("Invalid title, try again")
+        else:
+            return
         
