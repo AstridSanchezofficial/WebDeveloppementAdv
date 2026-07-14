7b2ae1757488d26193102c0f70a28acedc4d82ab
AstridSanchezofficial <135763279+AstridSanchezofficial@users.noreply.github.com>
2026-07-14 19:24:14 -0400

    Fixing bug 1
    : Connecting the data base with the app and creating table

--- Exam2/app.py
+++ Exam2/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///albums.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy
+db = SQLAlchemy(app)
 
 
 class Album(db.Model):
